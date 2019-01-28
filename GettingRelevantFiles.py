e306f2f89ecec2b111abc594b707f42612d45953
aulorbe <aulorbe@gmail.com>
2019-01-28 17:33:09 -0500

    getting info from invoices

--- GettingRelevantFiles.py
+++ GettingRelevantFiles.py
@@ -1,20 +1,19 @@
 # Looping through .txt invoices (the ones converted from pdf) & only grabbing them if they are 1) greater than 2 bytes 2) have the phrase 'Artist:' in them and/or '1stDibs Ref:'
-# import sys
-# reload(sys)
-# sys.setdefaultencoding("ISO-8859-1")
 
 import os
-import io
+import shutil
 
-files_with_artists = [] #412 unique files (figure out why you're writing duplicates of each file in the script below)
+folder = '/Users/flatironschool/Final-Project/Invoices_Txt'
 
-bad_files = [] #33 files
+files_with_artists = [] #412 unique files (figure out why you're writing duplicates of each file in the script below later)
+
+bad_files = [] #33 files -- deal with these later
 
 for file in os.listdir("/Users/flatironschool/Final-Project/Invoices_PDFsToText"):
     if os.path.getsize(f'/Users/flatironschool/Final-Project/Invoices_PDFsToText/{file}') > 2:
         with open(f'/Users/flatironschool/Final-Project/Invoices_PDFsToText/{file}', 'rb') as f:
             for line in f:
                 if 'Artist:' or '1stDibs Ref 'in line:
-                    files_with_artists.append(file)
+                    shutil.copy(f'/Users/flatironschool/Final-Project/Invoices_PDFsToText/{file}', folder)
     else:
         bad_files.append(file)
---
+++ MakingInvoiceDataframes.py
@@ -0,0 +1,66 @@
+# Making a list of dictionaries, where each dictionary is an invoice with all the data I need
+
+import re
+import os
+
+all_files = []
+
+for file in os.listdir("/Users/flatironschool/Final-Project/Invoices_Txt"):
+    all_files.append(f'/Users/flatironschool/Final-Project/Invoices_Txt/{file}')
+
+def make_dicts(files):
+
+    list_of_dicts = []
+
+    for file in files:
+
+        file_dict = {}
+
+        with open(file , 'rb') as f:
+
+            titles = []
+            artists = []
+            media = []
+            dimensions = []
+            dates = []
+
+            for line in f:
+
+                match_inv = re.search(b'(invoice| )(\# |\s)[0-9]*\-[0-9]*', line, re.IGNORECASE)
+                match_artist = re.search(b'^(Artist:)\s+(.+)', line, re.IGNORECASE)
+                match_title = re.search(b'^(Title:)\s+(.+)', line, re.IGNORECASE)
+                match_med = re.search(b'^(Medium:)\s+(.+)', line, re.IGNORECASE)
+                match_dims = re.search(b'^(Dimensions:)\s+(.+)', line, re.IGNORECASE)
+                match_datecreated = re.search(b'^(Date:\s+(.+))', line, re.IGNORECASE)
+                match_datesold = re.search(b'\d+\/\d*\/\d*', line, re.IGNORECASE)
+
+                if match_inv:
+                    file_dict['INVOICE ID'] = match_inv.group(0)
+
+                if match_artist:
+                    artists.append(match_artist.group(0)[8:])
+
+                if match_title:
+                    titles.append(match_title.group(0)[7:])
+
+                if match_med:
+                    media.append(match_med.group(0)[8:])
+
+                if match_dims:
+                    dimensions.append(match_dims.group(0)[12:])
+
+                if match_datecreated:
+                     dates.append(match_datecreated.group(0)[6:])
+
+                if match_datesold:
+                    file_dict['Date Sold'] = match_datesold.group(0)
+
+            file_dict['Titles'] = titles
+            file_dict['Artist'] = artists
+            file_dict['Medium'] = media
+            file_dict['Dimensions'] = dimensions
+            file_dict['Date Created by Artist'] = dates
+
+        list_of_dicts.append(file_dict)
+
+    return list_of_dicts
