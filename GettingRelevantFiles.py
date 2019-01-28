19956bbb2ace93db75eabfe7d08923d025b10a43
aulorbe <aulorbe@gmail.com>
2019-01-28 11:11:07 -0500

    bug fixes

--- GettingRelevantFiles.py
+++ GettingRelevantFiles.py
@@ -1,6 +1,10 @@
 # Looping through .txt invoices (the ones converted from pdf) & only grabbing them if they are 1) greater than 2 bytes 2) have the phrase 'Artist:' in them and/or '1stDibs Ref:'
+# import sys
+# reload(sys)
+# sys.setdefaultencoding("ISO-8859-1")
 
 import os
+import io
 
 files_with_artists = [] #412 unique files (figure out why you're writing duplicates of each file in the script below)
 
@@ -8,7 +12,7 @@ bad_files = [] #33 files
 
 for file in os.listdir("/Users/flatironschool/Final-Project/Invoices_PDFsToText"):
     if os.path.getsize(f'/Users/flatironschool/Final-Project/Invoices_PDFsToText/{file}') > 2:
-        with open(f'/Users/flatironschool/Final-Project/Invoices_PDFsToText/{file}') as f:
+        with open(f'/Users/flatironschool/Final-Project/Invoices_PDFsToText/{file}', 'rb') as f:
             for line in f:
                 if 'Artist:' or '1stDibs Ref 'in line:
                     files_with_artists.append(file)
